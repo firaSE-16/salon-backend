28ae60de2dadc42ced8b96d1cc80f2836b36667b
firaSE-16 <y.taddese@elunic.net>
2026-03-03 12:03:06 +0300

    Add allowed origin for Vercel deployment in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -6,6 +6,7 @@ return [
     'allowed_origins' => [
         'http://localhost:5173',
         'http://localhost:3000',
+        'https://salon-front-9map9pn9z-loariftech-4320s-projects.vercel.app',
     ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => [
